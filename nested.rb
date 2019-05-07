4e382e51b19597a6c6b66d85a475e726dd85479b
Rori Hinds <r_ori@hotmail.co.uk>
2019-05-07 23:47:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -113,6 +113,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:known_for] = "dead"
+     
      
      
      
